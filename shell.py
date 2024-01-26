2133385bbd088838c3e5ad80d36fbdb525e2ec8f
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2024-01-26 11:30:01 +0100

    feat: Added sigint (ctrl c) handeling

--- shell.py
+++ shell.py
@@ -3,6 +3,7 @@ import logging
 logger = logging.getLogger("taskmaster: " + __name__)
 logging.basicConfig()
 logger.setLevel(logging.DEBUG)
+import signal
 
 
 def command_interpreter(command: str):
@@ -13,11 +14,24 @@ def command_interpreter(command: str):
 			logger.info("stoping ...")
 		case "restart":
 			logger.info("restart")
+		case "status":
+			logger.info("status")
+		case "reload":
+			logger.info("reload")
+		case "exit":
+			logger.info("exit")
 		case _:
 			logger.info("Unknown command: " + command)
-			logger.info("Available commands: start, stop, restart")
+			logger.info("Available commands: start, stop, restart, reload, status, exit")
+
+def sigint_handler(signum, frame):
+	logger.info("\nSIGINT received, stopping...")
+	exit(0)
+
 
 def prompt_actions():
+	signal.signal(signal.SIGINT, sigint_handler)
+
 	while True:
 		command = input(">>> ")
 		command_interpreter(command)
