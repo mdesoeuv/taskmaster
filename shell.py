1de74005389aa8c060ff9cab60738bd01acda0e4
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2024-01-26 11:30:57 +0100

    Refactor command_interpreter function in shell.py

--- shell.py
+++ shell.py
@@ -7,32 +7,34 @@ import signal
 
 
 def command_interpreter(command: str):
-	match command:
-		case "start":
-			logger.info("starting ...")
-		case "stop":
-			logger.info("stoping ...")
-		case "restart":
-			logger.info("restart")
-		case "status":
-			logger.info("status")
-		case "reload":
-			logger.info("reload")
-		case "exit":
-			logger.info("exit")
-		case _:
-			logger.info("Unknown command: " + command)
-			logger.info("Available commands: start, stop, restart, reload, status, exit")
+    match command:
+        case "start":
+            logger.info("starting ...")
+        case "stop":
+            logger.info("stoping ...")
+        case "restart":
+            logger.info("restart")
+        case "status":
+            logger.info("status")
+        case "reload":
+            logger.info("reload")
+        case "exit":
+            logger.info("exit")
+        case _:
+            logger.info(
+                "Unknown command: `"
+                + command
+                + "` (Available commands: start, stop, restart, reload, status, exit)"
+            )
 
 def sigint_handler(signum, frame):
-	logger.info("\nSIGINT received, stopping...")
-	exit(0)
+    logger.info("\nSIGINT received, stopping...")
+    exit(0)
 
 
 def prompt_actions():
-	signal.signal(signal.SIGINT, sigint_handler)
+    signal.signal(signal.SIGINT, sigint_handler)
 
-	while True:
-		command = input(">>> ")
-		command_interpreter(command)
-	
+    while True:
+        command = input(">>> ")
+        command_interpreter(command)
