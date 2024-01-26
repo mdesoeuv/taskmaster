959a9f3f42a716a8351e7e84b08d68afac979512
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-01-26 09:36:45 +0100

    feat: yaml loading

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import logging
+import yaml
+import pathlib
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
 
 class TaskException(Exception):
+    def __init__(self, message):            
+        super().__init__(message)
+
+
+class ConfigError(TaskException):
     def __init__(self, message):            
         super().__init__(message)
 
@@ -10,9 +22,19 @@ logging.basicConfig()
 logger.setLevel(logging.DEBUG)
 
 
+def config_parser(path: pathlib.Path):
+    if not path.exists() or path.is_dir():
+        raise ConfigError("configuration file error.")
+    with open(path) as file:
+        config = yaml.load(file, Loader=Loader)
+        logger.info(config)
+
+
 def main():
     logger.info("Hello !")
-    raise TaskException("Error happening !")
+    config_parser(path=pathlib.Path("config.yaml"))
+    # raise TaskException("Error happening !")
+
 
 if __name__ == "__main__":
     try:
