fb29b0dabe00def6451b9cb36db4a9f7bd3a47b4
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2024-01-26 10:46:20 +0100

    Add logging and argument parsing to cli.py

--- cli.py
+++ cli.py
@@ -1,1 +1,18 @@
 import logging
+import argparse
+
+logger = logging.getLogger("taskmaster: " + __name__)
+logging.basicConfig()
+logger.setLevel(logging.DEBUG)
+
+
+def parse_arguments():
+	parser = argparse.ArgumentParser(description='Process taskmaster options.')
+	parser.add_argument('integers', metavar='N', type=int, nargs='+',
+						help='an integer for the accumulator')
+	parser.add_argument('--sum', dest='accumulate', action='store_const',
+						const=sum, default=max,
+						help='sum the integers (default: find the max)')
+
+	args = parser.parse_args()
+	print(args.accumulate(args.integers))
