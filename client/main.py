c32d43173187923f085576b2c8bdbb40c29446f3
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-03-04 18:11:55 +0100

    fix: simplified error log

--- client/main.py
+++ client/main.py
@@ -29,98 +29,111 @@ def is_command_valid(input_command: str) -> bool:
 async def display_loading(should_run: dict, timeout=5):
     start_time = asyncio.get_running_loop().time()
     counter = 0
-    while (asyncio.get_running_loop().time() - start_time) < timeout and should_run['waiting_for_response']:
+    while (
+        asyncio.get_running_loop().time() - start_time
+    ) < timeout and should_run["waiting_for_response"]:
         await asyncio.sleep(0.1)
         counter += 1
         if counter % 10 == 0:
             await aprint("loading...")
-    if should_run['waiting_for_response']:
+    if should_run["waiting_for_response"]:
         await aprint("No response from server. Please check your connection.")
-    should_run['waiting_for_response'] = False
+    should_run["waiting_for_response"] = False
 
 
 async def listen_from_server(reader, should_run: dict):
     try:
-        while not reader.at_eof() and should_run['connection_active']:
+        while not reader.at_eof() and should_run["connection_active"]:
             data = await reader.readline()
-            message = data.decode('utf-8')
+            message = data.decode("utf-8")
             if message:
-                should_run['waiting_for_response'] = False
+                should_run["waiting_for_response"] = False
                 if message.strip() == "server_shutdown":
-                    should_run['connection_active'] = False
+                    should_run["connection_active"] = False
                     break
-                await aprint(message, end='')
+                await aprint(message, end="")
     except Exception as e:
         logger.error(f"Error while listening for server messages: {e}")
 
 
 async def monitor_state(should_run: dict):
-    while should_run['connection_active']:
+    while should_run["connection_active"]:
         await asyncio.sleep(0.1)
 
 
 async def send_user_commands(writer, should_run: dict):
     try:
-        while should_run['connection_active']:
+        while should_run["connection_active"]:
             user_input_task = asyncio.create_task(ainput("> "))
             monitor_task = asyncio.create_task(monitor_state(should_run))
 
             done, pending = await asyncio.wait(
                 [user_input_task, monitor_task],
-                return_when=asyncio.FIRST_COMPLETED
+                return_when=asyncio.FIRST_COMPLETED,
             )
 
             for task in pending:
                 task.cancel()
 
-            if not should_run['connection_active']:
-                logger.info("\nExiting due to server shutdown or command termination.")
+            if not should_run["connection_active"]:
+                logger.info(
+                    "\nExiting due to server shutdown or command termination."
+                )
                 break
 
             if user_input_task in done:
                 command = user_input_task.result()
-                if command == '':
+                if command == "":
                     logger.info("EOF received. Closing connection...")
-                    should_run['connection_active'] = False
+                    should_run["connection_active"] = False
                     break
 
                 if is_command_valid(command):
-                    should_run['waiting_for_response'] = True
+                    should_run["waiting_for_response"] = True
                     writer.write(command.encode())
                     await writer.drain()
                     if command == "exit":
                         logger.info("Exiting...")
-                        should_run['connection_active'] = False
+                        should_run["connection_active"] = False
                         break
-                    loading_task = asyncio.create_task(display_loading(should_run))
+                    loading_task = asyncio.create_task(
+                        display_loading(should_run)
+                    )
                     await loading_task
-    except EOFError as e:
+    except EOFError:
         logger.info("EOF received. Closing connection...")
-        should_run['connection_active'] = False
+        should_run["connection_active"] = False
     except Exception as e:
-        logger.error(f"Error while sending user commands: {e.__class__.__name__}: {e}")
+        logger.error(f"Error while sending user commands: {e}")
     finally:
         writer.close()
         await writer.wait_closed()
 
+
 async def start_client(host, port):
-    should_run = {'connection_active': True, 'waiting_for_response': False}
+    should_run = {"connection_active": True, "waiting_for_response": False}
     reader, writer = None, None
 
     try:
         reader, writer = await asyncio.open_connection(host, port)
         logger.info("Connected to the server. Type 'exit' to exit.")
 
-        listener_task = asyncio.create_task(listen_from_server(reader, should_run))
-        user_input_task = asyncio.create_task(send_user_commands(writer, should_run))
+        listener_task = asyncio.create_task(
+            listen_from_server(reader, should_run)
+        )
+        user_input_task = asyncio.create_task(
+            send_user_commands(writer, should_run)
+        )
 
         await asyncio.gather(listener_task, user_input_task)
 
     except ConnectionRefusedError:
-        logger.error("Server is not running. Please start the server and try again.")
+        logger.error(
+            "Server is not running. Please start the server and try again."
+        )
     except KeyboardInterrupt:
         logger.info("Client interrupted by user. Closing connection...")
-        should_run['connection_active'] = False
+        should_run["connection_active"] = False
         if writer is not None:
             writer.close()
             await writer.wait_closed()
